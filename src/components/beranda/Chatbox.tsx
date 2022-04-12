ca41a014b5c6e7ad58a74b8f98e8d1d254ee5dc9
Muhammad Raihan Satrio <adewipo@gmail.com>
2022-04-12 22:12:23 +0700

    feat: added UI chatbot

---
+++ src/components/beranda/Chatbox.tsx
@@ -0,0 +1,57 @@
+export const Chatbox = () => {
+    return (
+        <section id="chat-section">
+            <div className="container">
+                <div className="center">
+                    <div className="grid-box-chat">
+                        <div>
+                            <div className="title-section">
+                                <h2 className="title-banner">Chatbot Penyuluhan</h2>
+                            </div>
+                            <div className="description-section">
+                                <p className="description-banner">Silakan bertanya berkaitan dengan Narkoba pada chatbot kami</p>
+                            </div>
+                        </div>
+                        <div className="chatbot">
+                            <div className="box">
+                                <div className="container">
+                                    <ul>
+                                        <li className="our-chat">
+                                            <span className="our-msg">Haosfjos fsss</span>
+                                        </li>
+                                        <li className="response-chat">
+                                            <span className="response-msg">Halssss</span>
+                                        </li>
+                                        <li className="our-chat">
+                                            <span className="our-msg">Hal ohal ha l oasjsf sfj osfosjf ofjsff sssssssssss</span>
+                                        </li>
+                                        <li className="response-chat">
+                                            <span className="response-msg">Hal ohal ha l oasjsf sfj osfosjf ofjs fosfjos fosjfo safffffff sssssssssss</span>
+                                        </li>
+                                        <li className="our-chat">
+                                            <span className="our-msg">Hal ohal ha l oasjsf sfj osfosjf ofjs fosfjos fosjfo safffffff sssssssssss</span>
+                                        </li>
+                                        <li className="response-chat">
+                                            <span className="response-msg">Hal ohal ha l oasjsf sfj osfosjf ofjs fosfjos fosjfo safffffff sssssssssss</span>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div className="flex items-center justify-between w-full p-3 border-t border-gray-300">
+                                    <input type="text" placeholder="Ketik Pesan"
+                                        className="block w-full py-2 pl-4 mx-3 bg-gray-100 rounded-full outline-none focus:text-gray-700"
+                                        name="message" required />
+                                    <button type="submit">
+                                        <svg className="w-5 h-5 text-gray-500 origin-center transform rotate-90" xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 20 20" fill="currentColor">
+                                        <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" />
+                                        </svg>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
--- src/pages/Beranda.tsx
+++ src/pages/Beranda.tsx
@@ -1,6 +1,7 @@
 import { BackgroundHeader } from "../components/beranda/BackgroundHeader";
 import { BenefitDua } from "../components/beranda/BenefitDua";
 import { BenefitSatu } from "../components/beranda/BenefitSatu";
+import { Chatbox } from "../components/beranda/Chatbox";
 import { CounterStat } from "../components/beranda/CounterStat";
 import { Testimonial } from "../components/beranda/Testimonial";
 import { WelcomeHeader } from "../components/beranda/WelcomeHeader";
@@ -11,6 +12,7 @@ export const Beranda = () => {
             <WelcomeHeader/>
             <BackgroundHeader/>
             <CounterStat/>
+            <Chatbox/>
             <BenefitSatu/>
             <BenefitDua/>
             <Testimonial/>
